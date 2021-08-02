37103ad3aa1a6389101f31e44a3c647d238c5411
Bruno <brunomesquita@MacBook-Pro-de-Bruno.local>
2021-08-01 23:27:40 -0300

    versao 1.0

---
+++ firstcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstcode
+//
+//  Created by Bruno Mesquita on 01/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
